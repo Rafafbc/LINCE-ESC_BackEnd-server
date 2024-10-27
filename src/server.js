45e7aa0d97758eb459c17ff298d121ab6015261e
Rafael Baldessini Campanini <rafaelcampanini90@gmail.com>
2024-10-27 16:52:22 -0300

    att port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 
 // Environment variables (from Railway settings)
 const db = mysql.createConnection({
