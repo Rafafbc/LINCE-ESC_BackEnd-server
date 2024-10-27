a19a919b9d827fd0d8fe49a84aeb1e0bb1c60c3a
Rafael Baldessini Campanini <rafaelcampanini90@gmail.com>
2024-10-27 16:24:48 -0300

    alterações no 'server.js'

--- src/server.js
+++ src/server.js
@@ -13,25 +13,36 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('Database connection error:', err);
+    console.error('Erro ao conectar ao MySQL:', err);
+    process.exit(1); // Encerra o processo se a conexão falhar
   } else {
-    console.log('Connected to MySQL database.');
+    console.log('Conectado ao MySQL.');
   }
 });
 
 app.use(express.json());
 
 app.post('/dados', (req, res) => {
-    const { temperatura, umidade, pressao, lux, timestamp } = req.body;
-  
-    const query = 'INSERT INTO sensor_data (temperatura, umidade, pressao, lux, timestamp) VALUES (?, ?, ?, ?, ?)';
-    db.query(query, [temperatura, umidade, pressao, lux, timestamp], (err, results) => {
-      if (err) {
-        console.error('Failed to insert data:', err);
-        res.status(500).send('Error saving data');
-      } else {
-        res.send('Data saved successfully');
-      }
-    });
+  const { temperatura, umidade, pressao, lux, timestamp } = req.body;
+
+  const query = 'INSERT INTO sensor_data (temperatura, umidade, pressao, lux, timestamp) VALUES (?, ?, ?, ?, ?)';
+  db.query(query, [temperatura, umidade, pressao, lux, timestamp], (err, results) => {
+    if (err) {
+      console.error('Erro ao inserir dados:', err);
+      res.status(500).send('Erro ao inserir dados.');
+    } else {
+      res.send('Dados recebidos e salvos com sucesso!');
+    }
   });
-  
+});
+
+app.get('/dados', (req, res) => {
+  const query = 'SELECT * FROM sensor_data';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar dados:', err);
+      return res.status(500).send('Erro ao buscar dados.');
+    }
+    res.json(results);
+  });
+});
